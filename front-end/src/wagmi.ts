7746541f7b2a91e5446ea9259a53c24be138fa61
Heblishli <heblishli@MacBook-Air.local>
2023-10-06 09:08:51 +0200

    fixing submodule

---
+++ front-end/src/wagmi.ts
@@ -0,0 +1,23 @@
+import { w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { configureChains, createConfig } from 'wagmi'
+import { goerli, mainnet } from 'wagmi/chains'
+
+export const walletConnectProjectId = '73e7f958b2e2dc4dcaf0922d9bc0cba7'
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet, ...(import.meta.env?.MODE === 'development' ? [goerli] : [])],
+  [w3mProvider({ projectId: walletConnectProjectId })],
+)
+
+export const config = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({
+    chains,
+    projectId: walletConnectProjectId,
+    version: 2,
+  }),
+  publicClient,
+  webSocketPublicClient,
+})
+
+export { chains }
